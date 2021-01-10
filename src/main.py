0428df535daa948a8f09f4a7f885ef0a4fd3733a
Kunal Roychoudhury <lanuks97@gmail.com>
2021-01-10 19:22:53 +0530

    started 'main' implementation

--- src/main.py
+++ src/main.py
@@ -13,10 +13,23 @@ import encryptor
 def main(argv: list) -> int:
     r"""Customary Main Function \
     argv : list : command line argument list"""
+
+    if len(argv) == 0:
+        error_message: str = """Expected arguments as follows:
+encrypt <directory_path> <destination_file_name> <destination_path>
+decrypt <target_file_name> <destination_path>"""
+        raise ValueError(error_message)
+
+
     raise NotImplementedError("Function yet to be implemented")
 
 
 if __name__ == "__main__":
     # Obtain and parse arguments from command line
     # perform corresponding actions
-    main(sys.argv[1:])
+    try:
+        main(sys.argv[1:])
+    except Exception as error:
+        print(type(error), error)
+    finally:
+        print(" -- execution complete -- ")
